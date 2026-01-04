358102799d769138a8a67191c45bb31e7ede1828
Rutikesh <76697103+Rutikesh@users.noreply.github.com>
2026-01-04 16:34:33 +0530

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,3 +1,3 @@
 def call(String Projectname, String ImageTag, String DockerHubUser){
     echo 'Building Docker image'
-    sh "docker build -t ${DockerHubUser}/${Projectname}:${ImageTag}
+    sh "docker build -t ${DockerHubUser}/${Projectname}:${ImageTag}"
